045ebc034b12c8b40541e09cb657f475614aaa8a
Ooblek <silesh@urbanstopstudio.co>
2020-10-02 19:59:21 +0530

    swapped out placeholders, added a loader thing

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
+// bezier path for each satellite
+
 const flightPath = {
     curviness: 1.25,
-    autoRotate:true,
+    autoRotate:false,
     values: [
         {x:400,y:-20},
-        {x:window.innerWidth - 250,y:-20},
+        {x:window.outerWidth - window.outerWidth/4,y:-20},
+        // {x:window.outerWidth - 350,y:-20},
+
+        
+        
+
+    ]
+}
+
+const flightPath2= {
+    curviness: 1.25,
+    autoRotate:false,
+    values: [
+        {x:400,y:-20},
+        {x:window.outerWidth - window.outerWidth/2.3,y:-150},
+        // {x:window.outerWidth - 350,y:-20},
+
+        
+        
+
+    ]
+}
+
+const flightPath3= {
+    curviness: 1.25,
+    autoRotate:false,
+    values: [
+        {x:400,y:-20},
+        {x:window.outerWidth - window.outerWidth/1.9,y:150},
+        // {x:window.outerWidth - 350,y:-20},
+
+        
+        
+
+    ]
+}
+
+const flightPath4= {
+    curviness: 1.25,
+    autoRotate:false,
+    values: [
+        {x:400,y:-20},
+        {x:window.outerWidth - window.outerWidth/1.5,y:-130},
+        // {x:window.outerWidth - 350,y:-20},
 
         
         
@@ -12,6 +57,17 @@ const flightPath = {
 }
 
 
+
+
+
+const movePath = {
+    curviness:1.25,
+    autoRotate:false,
+    values: [
+        {x:400,y:-20}
+    ]
+}
+
 const tween = new TimelineLite();
 
 tween.add(
@@ -19,6 +75,7 @@ tween.add(
     TweenLite.to('.paper',1, {
         // bezier: flightPath,
         scale:100,
+        opacity:0,
 
         
         ease:Power1.easeInOut
@@ -31,6 +88,33 @@ tween.add(
         bezier: flightPath,
         scale:2,
     })
+    
+    
+)
+tween.add(
+    TweenLite.to('.satellite2',1, {
+        bezier: flightPath2,
+        scale:1.3,
+    })
+)
+
+
+tween.add(
+    TweenLite.to('.satellite3',1, {
+        bezier: flightPath3,
+        scale:2,
+    }, 0)
+    
+    
+)
+
+tween.add(
+    TweenLite.to('.satellite4',1, {
+        bezier: flightPath4,
+        scale:2,
+    }, 0)
+    
+    
 )
 
 const controller = new ScrollMagic.Controller();
@@ -45,10 +129,37 @@ const scene = new ScrollMagic.Scene({
 .addTo(controller)
 
 
+// Click events
+let aqua = document.getElementById('aqua')
+let aura = document.getElementById('aura')
+let loader = document.getElementById('loader')
 
-let satellite = document.getElementById('satellite')
 
 
-satellite.onclick = () => {
-    alert("I am a satellite")
+aqua.onclick = () => {
+    loader.classList.add('anim')
+    setTimeout(() => {window.location.href = './includes/aqua.html'
+    loader.classList.remove('anim')
+
+}, 1000)
+    
 }
+
+aura.onclick = () => {
+    alert("I am aura")
+  
+}
+
+calipso.onclick = () => {
+    alert("I am calipso")
+}
+
+cloudsat.onclick = () => {
+    alert("I am cloudsat")
+}
+
+// Page animations
+
+var x = document.getElementsByClassName('smoothLink')
+
+
